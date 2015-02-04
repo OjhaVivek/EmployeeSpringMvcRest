b446cbaa69e1ed6bf88232261e215846efaf27b9
OjhaVivek <vvkojha06@gmail.com>
2015-02-04 23:46:08 +0530

    added sql dump for the database

---
+++ employee_database.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Community v11.31 (64 bit)
+MySQL - 5.6.17 : Database - employee_database
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`employee_database` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `employee_database`;
+
+/*Table structure for table `employee` */
+
+DROP TABLE IF EXISTS `employee`;
+
+CREATE TABLE `employee` (
+  `employee_id` int(11) NOT NULL AUTO_INCREMENT,
+  `employee_name` varchar(255) NOT NULL,
+  `salary` bigint(20) NOT NULL,
+  PRIMARY KEY (`employee_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `employee` */
+
+insert  into `employee`(`employee_id`,`employee_name`,`salary`) values (1,'Vivek',150000000),(2,'Arjun',100000000),(3,'Anusha Rajan',10000000),(4,'Deepika Padukone',14000000),(5,'Rajat Bhatia',100000000),(6,'Barack Obama',10000001);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
